3459bc3043a9e3712bb3a217eb16fe2ec5407169
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-01-18 19:05:22 +0100

    only update badges  of active tab

--- background.js
+++ background.js
@@ -12,8 +12,12 @@ const handleFocusMode = async (tab) => {
     if (tab.url && tab.url !== 'https://www.powerpyx.com/' && tab.url.startsWith('https://www.powerpyx.com/')) {
         await chrome.action.setBadgeText({
             text: "ON",
+            tabId: tab.id,
         });
-        await chrome.action.setBadgeBackgroundColor({ color: '#2a9d8f' });
+        await chrome.action.setBadgeBackgroundColor({ 
+            color: '#2a9d8f',
+            tabId: tab.id,
+        });
 
         await chrome.scripting.insertCSS({
             files: ["focus-mode.css"],
@@ -25,5 +29,6 @@ const handleFocusMode = async (tab) => {
 
     await chrome.action.setBadgeText({
         text: "",
+        tabId: tab.id,
     });
 };
