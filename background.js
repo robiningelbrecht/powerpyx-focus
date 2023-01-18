4e99f14a0b37e62dc906c5a151f4ab5f206db665
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-01-18 18:56:24 +0100

    Simpler and better appraoch

--- background.js
+++ background.js
@@ -1,79 +1,29 @@
-const green = '#2a9d8f';
-const red = '#e63946';
-chrome.runtime.onInstalled.addListener(async () => {
-    await chrome.action.setBadgeText({
-        text: " ",
-    });
-    await chrome.action.setBadgeBackgroundColor({color: red});
-
-    chrome.storage.local.set({ focusModeEnabled: false });
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    handleFocusMode(tab);
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (!tab.url) {
-        return;
-    }
-    if (!tab.url.startsWith('https://www.powerpyx.com')) {
-        return;
-    }
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    const tab = await chrome.tabs.get(activeInfo.tabId);
 
-    chrome.storage.sync.get("focusModeEnabled").then(async ({ focusModeEnabled }) => {
+    handleFocusMode(tab);
+});
+
+const handleFocusMode = async (tab) => {
+    if (tab.url && tab.url !== 'https://www.powerpyx.com/' && tab.url.startsWith('https://www.powerpyx.com/')) {
         await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: ' ',
+            text: "ON",
         });
+        await chrome.action.setBadgeBackgroundColor({ color: '#2a9d8f' });
 
-        if (focusModeEnabled) {
-            await chrome.action.setBadgeBackgroundColor({color: green});
-            await chrome.scripting.insertCSS({
-                files: ["focus-mode.css"],
-                target: { tabId: tab.id, allFrames: true },
-            });
-
-            return;
-        }
-
-        await chrome.action.setBadgeBackgroundColor({color: red});
-        await chrome.scripting.removeCSS({
+        await chrome.scripting.insertCSS({
             files: ["focus-mode.css"],
-            target: { tabId: tab.id, allFrames: true },
+            target: { tabId: tab.id },
         });
-    });
-});
-
-chrome.action.onClicked.addListener(async (tab) => {
-    if (!tab.url.startsWith('https://www.powerpyx.com')) {
         return;
     }
 
-    chrome.storage.sync.get("focusModeEnabled").then(async ({ focusModeEnabled }) => {
-        // Next state will always be the opposite
-        const nextState = !focusModeEnabled;
 
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: ' ',
-        });
-
-        chrome.storage.sync.set({ focusModeEnabled: nextState }).then(async () => {
-            if (nextState) {
-                await chrome.action.setBadgeBackgroundColor({color: green});
-                await chrome.scripting.insertCSS({
-                    files: ["focus-mode.css"],
-                    target: { tabId: tab.id, allFrames: true },
-                });
-
-                return;
-            }
-
-            await chrome.action.setBadgeBackgroundColor({color: red});
-            await chrome.scripting.removeCSS({
-                files: ["focus-mode.css"],
-                target: { tabId: tab.id, allFrames: true },
-            });
-        });
-
-
+    await chrome.action.setBadgeText({
+        text: "",
     });
-});
+};
